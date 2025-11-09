ebe397950c5c28f05a88351d70e64980280f04c6
kaseris <mich.kaseris@gmail.com>
2025-11-09 18:39:45 +0200

    fix(config): remove hardcoded paths

--- src/config.py
+++ src/config.py
@@ -1,17 +1,22 @@
+import os
+
 from dataclasses import dataclass
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 @dataclass
 class UpscalerSettings:
     ADCSR_SDXL_MODEL_NAME = "stabilityai/stable-diffusion-2-1-base"
-    ADCSR_DECODER_WEIGHTS_PATH = "weights/halfDecoder.ckpt"
-    ADCSR_MODEL_WEIGHTS_PATH = "weights/net_params_200.pkl"
+    ADCSR_DECODER_WEIGHTS_PATH = os.getenv("ADCSR_DECODER_WEIGHTS_PATH")
+    ADCSR_MODEL_WEIGHTS_PATH = os.getenv("ADCSR_MODEL_WEIGHTS_PATH")
 
 
 @dataclass
 class ServerSettings:
-    HOST = "0.0.0.0"
-    PORT = 8000
+    HOST = os.getenv("HOST", "0.0.0.0")
+    PORT = int(os.getenv("PORT", "8000"))
 
 
 upscaler_settings = UpscalerSettings()
