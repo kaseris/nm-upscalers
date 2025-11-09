9019e2bf92477567a613b17ba45a4d8716b4a8ba
kaseris <mich.kaseris@gmail.com>
2025-11-09 18:38:57 +0200

    chore(experimental): model client loading on startup

--- src/main.py
+++ src/main.py
@@ -11,6 +11,8 @@ sys.path.insert(0, CURR_DIR)
 
 from src import __version__
 from src.config import server_settings
+from src.upscalers.providers.adcsr.helpers import ADCSRWrapper
+from src.logger import logger
 
 app = FastAPI(title="Neural Moon Upscalers Endpoint", version=__version__)
 
@@ -22,8 +24,11 @@ async def root():
 
 @app.get("/health", response_class=PlainTextResponse)
 async def health():
+    logger.info("Health check request received")
     return "OK"
 
 
 if __name__ == "__main__":
+    logger.info(f"Starting server on {server_settings.HOST}:{server_settings.PORT}")
+    wrapper = ADCSRWrapper()
     uvicorn.run(app, host=server_settings.HOST, port=server_settings.PORT)
